f7d3560cdda3a21985acb982dfbacf2a93cb5317
donghae <todonghae@gmail.com>
2023-03-29 11:09:26 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const searchEl = document.querySelector('.search')
-const searchInputEl = searchEl.querySelector('input')
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-})  
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-})
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-})
-
-
 const badgeEL = document.querySelector('header .badges');
 // .throttle(함수, 시간ms
 window.addEventListener('scroll', _.throttle(function () {
@@ -159,8 +141,3 @@ spyEls.forEach(function (spyEl) {
   // 특정한 요소를 감시하는 요소를 만들어주는 메소드
   // 클래스를 추가, 제거하는 역할
 });
-
-
-// 현재 연도 구하기
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
